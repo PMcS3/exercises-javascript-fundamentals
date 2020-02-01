4f16c0237441aa4a99de86f91cf2609600b89c3a
Patrick Sullivan <pasullivan@davidson.edu>
2020-01-31 23:26:27 -0800

    any positive

--- exercises/arrays/anyPositive/anyPositive.js
+++ exercises/arrays/anyPositive/anyPositive.js
@@ -1,27 +1,41 @@
-/**
- * Given an array of numbers, returns true if _any_ element is positive
- * and false otherwise.
- *
- * @example
- * anyPositive([1, 2, 3, 4, 5]); // => true
- * anyPositive([1, 2, -3, 4, 5]); // => true
- * anyPositive([0, 0, 1]); // => true
- * anyPositive([-10, -10, -10]); // => false
- * anyPositive([-10, -10, 1]); // => true
- *
- * @param {number[]} array - An array of numbers
- * @returns {boolean} True if every number in the input array is positive
- *   and false otherwise.
- */
+let pennies = [-1,-1,-1,-1,-1,-1,-1,-1,-1];
+let dimes = [10,10,-10];
+let dollars = [100,100,200,3000,0,0,0];
+let nickels = [-5,-5,-5,0,0];
+let quarters = [-25,25,-25,25,-25,25];
+let zero = [0];
+
 function anyPositive(array) {
-  // This is your job. :)
+
+  let answer = false;
+  for(let i = 0; i<array.length;i++)
+  {
+    if(array[i] > 0)
+    {
+      answer = true;
+    }
+  }
+  return answer;
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for anyPositive:');
-
+function check(array)
+{
+ let verdict = anyPositive(array);
+ return verdict;
+}
+}
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
+if (require.main === module) {
+  console.log('Running sanity checks for allPositive:');
+  console.log(check(pennies) === false);
+  console.log(check(dimes) === true);
+  console.log(check(dollars) === true);
+  console.log(check(nickels) === false);
+  console.log(check(quarters) === true);
+  console.log(check(zero) === false);
 }
 
-module.exports = anyPositive;
+module.export = anyPositive;
