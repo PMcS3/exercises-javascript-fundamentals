54f8729053641f7d40de14327e374b56c635cdc8
Patrick Sullivan <pasullivan@davidson.edu>
2020-02-02 21:10:44 -0800

    sum of array

--- exercises/arrays/sum/sum.js
+++ exercises/arrays/sum/sum.js
@@ -9,14 +9,19 @@
  * @returns {number} The sum of the numbers in the array
  */
 function sum(array) {
-  // This is your job. :)
+  let number = 0;
+  for (let i = 0 ; i < array.length; i++)
+  {
+    number = number + array[i];
+  }
+  return number;
 }
 
 if (require.main === module) {
-  console.log('Running sanity checks for sum:');
-
-  // Add your own sanity checks here.
-  // How else will you be sure your code does what you think it does?
+  console.log('Running sanity checks for product:');
+  console.log(sum([2, 4, 6]) === 12);
+  console.log(sum([2, 4, 6]));
+  console.log(sum([-10, 10]) === 0);
+  console.log(sum([-10, 10]));
 }
-
 module.exports = sum;
